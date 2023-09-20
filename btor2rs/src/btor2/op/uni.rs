4a257af4674120754bcfe5ea17f2a468191ece28
Jan Onderka <onderjan@fit.cvut.cz>
2023-09-20 21:38:02 +0200

    Implement redand and redor

--- btor2rs/src/btor2/op/uni.rs
+++ btor2rs/src/btor2/op/uni.rs
@@ -44,8 +44,22 @@ impl UniOp {
                 Ok(quote!((#a_tokens) - (#one)))
             }
             UniOpType::Neg => Ok(quote!(-(#a_tokens))),
-            UniOpType::Redand => todo!(),
-            UniOpType::Redor => todo!(),
+            UniOpType::Redand => {
+                // equality with all ones
+                let all_ones_const = Const::new(true, 1);
+                let all_ones_tokens = all_ones_const.create_tokens(bitvec);
+
+                Ok(quote!(::machine_check_types::TypedEq::typed_eq(#a_tokens, #all_ones_tokens)))
+            }
+            UniOpType::Redor => {
+                // inequality with all zeros
+                let all_zeros_const = Const::new(false, 0);
+                let all_zeros_tokens = all_zeros_const.create_tokens(bitvec);
+
+                Ok(
+                    quote!(!(::machine_check_types::TypedEq::typed_eq(#a_tokens, #all_zeros_tokens))),
+                )
+            }
             UniOpType::Redxor => todo!(),
         }
     }
