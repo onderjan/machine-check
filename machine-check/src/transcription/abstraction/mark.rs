b0e553917f58c53a187daa1439047384c17b62a7
Jan Onderka <onderjan@fit.cvut.cz>
2023-09-26 21:06:04 +0200

    Remove no longer needed mark_impl

--- machine-check/src/transcription/abstraction/mark.rs
+++ machine-check/src/transcription/abstraction/mark.rs
@@ -1,13 +1,14 @@
 use proc_macro2::Span;
-use syn::{token::Brace, Ident, Item, ItemMod, ItemStruct};
+use syn::{token::Brace, Ident, ImplItem, Item, ItemImpl, ItemMod, ItemStruct};
 
 use crate::transcription::util::path_rule::{self, PathRule, PathRuleSegment};
 
-use self::mark_impl::transcribe_impl;
+use anyhow::anyhow;
+
+use self::mark_fn::transcribe_impl_item_fn;
 
 mod mark_fn;
 mod mark_ident;
-mod mark_impl;
 mod mark_stmt;
 mod mark_type_path;
 
@@ -16,7 +17,7 @@ pub fn transcribe(file: &mut syn::File) -> anyhow::Result<()> {
     for item in &file.items {
         let transcribed_item = match item {
             Item::Struct(s) => Item::Struct(transcribe_item_struct(s)?),
-            Item::Impl(i) => Item::Impl(transcribe_impl(i)?),
+            Item::Impl(i) => Item::Impl(transcribe_item_impl(i)?),
             _ => {
                 return Err(anyhow::anyhow!("Item type {:?} not supported", item));
             }
@@ -41,6 +42,24 @@ fn transcribe_item_struct(s: &ItemStruct) -> anyhow::Result<ItemStruct> {
     let mut s = s.clone();
     path_rule::transcribe_item_struct(&mut s, path_rules())?;
     Ok(s)
+}
+
+fn transcribe_item_impl(i: &ItemImpl) -> anyhow::Result<ItemImpl> {
+    let mut i = i.clone();
+    let mut items = Vec::<ImplItem>::new();
+
+    for item in i.items {
+        if let ImplItem::Fn(item_fn) = item {
+            let mut mark_fn = item_fn.clone();
+            transcribe_impl_item_fn(&mut mark_fn, i.self_ty.as_ref())?;
+            items.push(ImplItem::Fn(mark_fn));
+        } else {
+            return Err(anyhow!("Impl item type {:?} not supported", item));
+        };
+    }
+
+    i.items = items;
+    Ok(i)
 }
 
 fn path_rules() -> Vec<PathRule> {
