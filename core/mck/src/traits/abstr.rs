onderjan/machine-check

commit 3e36764d16f5543288ac6c44cf06b5b063ca9368

Add state fields display in WASM GUI
This should conclude porting of current GUI functionality from Javascript. However, there is a bug in state space visualisation, where multiple things map to the same tile.

core/mck/src/traits/abstr.rs
@@ -1,6 +1,9 @@
-use crate::abstr::PanicResult;
+use serde::{Deserialize, Serialize};
+
+use crate::abstr::{format_zeros_ones, PanicResult};
 use crate::concr::FullMachine;
-use std::fmt::Debug;
+use std::collections::BTreeMap;
+use std::fmt::{Debug, Display};
 use std::hash::Hash;
 
 use super::misc::MetaEq;
@@ -46,6 +49,39 @@ where
     fn uninit() -> Self;
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+pub struct BitvectorField {
+    pub bit_width: u32,
+    pub ones: u64,
+    pub zeros: u64,
+}
+
+impl Display for BitvectorField {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        format_zeros_ones(f, self.bit_width, self.ones, self.zeros)
+    }
+}
+
+#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
+pub struct ArrayFieldBitvector {
+    pub ones: u64,
+    pub zeros: u64,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ArrayField {
+    pub bit_width: u32,
+    pub bit_length: u32,
+    // TODO: u64 keys
+    pub inner: BTreeMap<String, ArrayFieldBitvector>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum Field {
+    Bitvector(BitvectorField),
+    Array(ArrayField),
+}
+
 pub trait ManipField {
     fn index(&self, index: u64) -> Option<&dyn ManipField>;
     fn num_bits(&self) -> Option<u32>;
@@ -53,7 +89,7 @@ pub trait ManipField {
     fn max_unsigned(&self) -> Option<u64>;
     fn min_signed(&self) -> Option<i64>;
     fn max_signed(&self) -> Option<i64>;
-    fn json_description(&self) -> serde_json::Value;
+    fn description(&self) -> Field;
 }
 pub trait Manipulatable {
     #[must_use]

core/mck/src/array/abstr.rs
@@ -1,11 +1,8 @@
-use std::fmt::Debug;
-
-use serde_json::json;
+use std::{collections::BTreeMap, fmt::Debug};
 
 use crate::{
-    abstr::{self, Abstr, ManipField, Phi},
-    concr,
-    concr::UnsignedBitvector,
+    abstr::{self, Abstr, ArrayField, Field, ManipField, Phi},
+    concr::{self, UnsignedBitvector},
     forward::ReadWrite,
     misc::MetaWrap,
     traits::misc::MetaEq,
@@ -145,19 +142,19 @@ impl<const I: u32, const L: u32> ManipField for Array<I, L> {
         None
     }
 
-    fn json_description(&self) -> serde_json::Value {
-        let mut map = serde_json::Map::new();
+    fn description(&self) -> Field {
+        let mut inner = BTreeMap::new();
         for (index, element) in self.inner.light_iter() {
-            map.insert(index.to_string(), element.0.json_domains());
+            inner.insert(
+                index.as_bitvector().as_unsigned().to_string(),
+                element.0.element_description(),
+            );
         }
 
-        let result = json!({
-            "type": "array",
-            "bit_width": L,
-            "bit_length": I,
-            "map": map,
-        });
-
-        result
+        Field::Array(ArrayField {
+            bit_width: L,
+            bit_length: I,
+            inner,
+        })
     }
 }

core/mck/src/bitvector/three_valued/abstr.rs
@@ -16,3 +16,5 @@ pub struct ThreeValuedBitvector<const L: u32> {
     zeros: ConcreteBitvector<L>,
     ones: ConcreteBitvector<L>,
 }
+
+pub(crate) use support::format_zeros_ones;

core/machine-check-gui/Cargo.toml
@@ -19,6 +19,8 @@ build-log-debug = []
 # Note that we need to be also able to compile the frontend 
 # using the backend architecture, so this includes dependencies 
 # that are frontend-only in practice.
+mck = "=0.3.1"
+
 anyhow = "1.0.89"
 wasm-bindgen = "0.2.100"
 wasm-bindgen-futures = "0.4.50"
@@ -27,6 +29,9 @@ web-sys = { version = "0.3.76", features = [
     "Window",
     "Document",
     "HtmlElement",
+    "HtmlTableElement",
+    "HtmlTableRowElement",
+    "HtmlTableCellElement",
     "Event",
     "MouseEvent",
     "Node",
@@ -39,6 +44,7 @@ web-sys = { version = "0.3.76", features = [
     "Response",
     "DomRect",
     "CssStyleDeclaration",
+    "DomTokenList",
 ] }
 console_error_panic_hook = "0.1.7"
 
@@ -51,7 +57,6 @@ bimap = "0.6.3"
 [target.'cfg(not(target_arch = "wasm32"))'.dependencies]
 # Backend-only dependencies.
 machine-check-common = "=0.3.1"
-mck = "=0.3.1"
 machine-check-exec = "=0.3.1"
 
 petgraph = "0.6.4"
@@ -60,7 +65,7 @@ petgraph = "0.6.4"
 log = "0.4.20"
 env_logger = "0.10.0"
 
-wry = "0.43.1"
+wry = { version = "0.43.1", features = ["devtools"] }
 tao = "0.30.0"
 http = "1.1.0"
 mime_guess = "2.0.5"

core/machine-check-gui/src/backend.rs
@@ -221,7 +221,7 @@ fn framework_response<M: FullMachine>(
             for field_name in state_field_names.iter() {
                 let field_get = mck::abstr::Manipulatable::get(&panic_result.result, field_name)
                     .expect("Field name should correspond to a field");
-                let description = field_get.json_description();
+                let description = field_get.description();
 
                 fields.insert(field_name.clone(), description);
             }

core/machine-check-gui/src/frontend.rs
@@ -1,5 +1,4 @@
 pub mod content;
-mod local;
 mod update;
 
 use wasm_bindgen::prelude::*;
@@ -11,11 +10,11 @@ pub async fn exec() {
 
     setup_listeners();
 
-    update::render(true).await;
+    update::display(true).await;
 }
 
 pub async fn resize() {
-    update::render(true).await;
+    update::display(true).await;
 }
 
 pub async fn step_verification() {

core/machine-check-gui/src/frontend/content.rs
@@ -2,6 +2,8 @@ use std::collections::{BTreeMap, BTreeSet};
 
 use serde::{Deserialize, Serialize};
 
+use mck::abstr::Field;
+
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ThreeValuedBool {
     pub zero: bool,
@@ -13,7 +15,7 @@ pub struct Node {
     pub incoming: BTreeSet<String>,
     pub outgoing: BTreeSet<String>,
     pub panic: Option<ThreeValuedBool>,
-    pub fields: BTreeMap<String, serde_json::Value>,
+    pub fields: BTreeMap<String, Field>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]

core/machine-check-gui/src/frontend/update.rs
@@ -1,3 +1,4 @@
+mod fields;
 mod mouse;
 mod render;
 mod view;
@@ -47,26 +48,28 @@ thread_local! {
 
 pub async fn update(action: Action, resize: bool) {
     execute_action(action).await;
-    render_current(resize);
+    display_current(resize);
 }
 
-pub async fn render(resize: bool) {
+pub async fn display(resize: bool) {
     let should_execute = VIEW.with(|view| view.borrow().is_none());
     if should_execute {
         execute_action(Action::GetContent).await;
     }
 
-    render_current(resize);
+    display_current(resize);
 }
 
-fn render_current(resize: bool) {
+fn display_current(resize: bool) {
     VIEW.with(|view| {
         let view_guard = view.borrow();
         let Some(ref view) = *view_guard else {
             panic!("View should be loaded");
         };
         POINT_OF_VIEW.with(|point_of_view| {
-            render::render(view, &point_of_view.borrow(), resize);
+            let point_of_view = point_of_view.borrow();
+            fields::display(view, &point_of_view);
+            render::render(view, &point_of_view, resize);
         });
     });
 }
@@ -77,6 +80,9 @@ async fn execute_action(action: Action) {
         Ok(ok) => ok,
         Err(err) => panic!("{:?}", err),
     };
+    let cons = Array::new_with_length(1);
+    cons.set(0, JsValue::from_str(&format!("Received JSON: {:?}", json)));
+    web_sys::console::log(&cons);
     let content: Content =
         serde_wasm_bindgen::from_value(json).expect("Content should be convertible from JSON");
 
@@ -124,6 +130,6 @@ pub async fn on_mouse(mouse: super::MouseEvent, event: web_sys::Event) {
     });
 
     if render {
-        render_current(true);
+        display_current(true);
     }
 }

commit 00a63f3247dc8e7c1621ba8f2e1afd97d4f8a2ac

Fix bug where the GUI showed flipped field zeros and ones
This was introduced in 3e36764d16f5543288ac6c44cf06b5b063ca9368 and only affected the GUI as the problem was in the access through a dynamic field.

core/mck/src/traits/abstr.rs
@@ -52,13 +52,13 @@ where
 #[derive(Debug, Serialize, Deserialize)]
 pub struct BitvectorField {
     pub bit_width: u32,
-    pub ones: u64,
     pub zeros: u64,
+    pub ones: u64,
 }
 
 impl Display for BitvectorField {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        format_zeros_ones(f, self.bit_width, self.ones, self.zeros)
+        format_zeros_ones(f, self.bit_width, self.zeros, self.ones)
     }
 }
 