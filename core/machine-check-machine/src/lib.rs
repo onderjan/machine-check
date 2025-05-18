onderjan/machine-check

commit 7630b1523d6cbbb5b5d0efaa5b00f3341c77344f

Update description machine names to fit better

core/machine-check-machine/src/lib.rs
@@ -94,7 +94,7 @@ fn process_items(items: &mut Vec<Item>) -> Result<(), MachineErrors> {
     #[cfg(feature = "write_machine")]
     let out_dir = out_dir();
 
-    let ssa_machine = description::create_ssa_machine(items.clone())?;
+    let description = description::create_description(items.clone())?;
 
     #[cfg(feature = "write_machine")]
     if let Some(out_dir) = &out_dir {
@@ -102,7 +102,7 @@ fn process_items(items: &mut Vec<Item>) -> Result<(), MachineErrors> {
             .expect("SSA machine file should be writable");
     }
 
-    let mut abstract_machine = abstr::create_abstract_machine(&ssa_machine)?;
+    let mut abstract_description = abstr::create_abstract_description(&description)?;
 
     #[cfg(feature = "write_machine")]
     if let Some(out_dir) = &out_dir {
@@ -110,18 +110,18 @@ fn process_items(items: &mut Vec<Item>) -> Result<(), MachineErrors> {
             .expect("Abstract machine file should be writable");
     }
 
-    let refinement_machine =
-        refin::create_refinement_machine(&abstract_machine).map_err(MachineError::from)?;
+    let refinement_description =
+        refin::create_refinement_description(&abstract_description).map_err(MachineError::from)?;
 
     // create new module at the end of the file that will contain the refinement
-    let refinement_module = create_machine_module("__mck_mod_refin", refinement_machine);
-    abstract_machine.items.push(refinement_module);
+    let refinement_module = create_machine_module("__mck_mod_refin", refinement_description);
+    abstract_description.items.push(refinement_module);
 
-    support::strip_machine::strip_machine(&mut abstract_machine)?;
+    support::strip_machine::strip_machine(&mut abstract_description)?;
 
-    concr::process_items(items, &ssa_machine.panic_messages)?;
+    concr::process_items(items, &description.panic_messages)?;
 
-    let abstract_module = create_machine_module("__mck_mod_abstr", abstract_machine);
+    let abstract_module = create_machine_module("__mck_mod_abstr", abstract_description);
     items.push(abstract_module);
 
     redirect_mck(items)?;

core/machine-check-machine/src/abstr.rs
@@ -17,13 +17,12 @@ use self::{
 
 use super::MachineError;
 
-pub(crate) fn create_abstract_machine(
-    ssa_machine: &MachineDescription,
+pub(crate) fn create_abstract_description(
+    description: &MachineDescription,
 ) -> Result<MachineDescription, MachineError> {
-    // expecting the concrete machine in SSA form
-    let mut abstract_machine = ssa_machine.clone();
+    let mut abstract_description = description.clone();
     // apply transcription to types using path rule transcriptor
-    match path_rules().apply_to_items(&mut abstract_machine.items) {
+    match path_rules().apply_to_items(&mut abstract_description.items) {
         Ok(()) => {}
         Err(err) => {
             return Err(MachineError::new(
@@ -36,7 +35,7 @@ pub(crate) fn create_abstract_machine(
     let mut machine_types = Vec::new();
     let mut processed_items = Vec::new();
 
-    for item in abstract_machine.items.iter() {
+    for item in abstract_description.items.iter() {
         if let Item::Impl(item_impl) = item {
             if let Some(ty) = preprocess_item_impl(item_impl)? {
                 machine_types.push(ty);
@@ -44,7 +43,7 @@ pub(crate) fn create_abstract_machine(
         }
     }
 
-    for item in abstract_machine.items.drain(..) {
+    for item in abstract_description.items.drain(..) {
         match item {
             syn::Item::Impl(item_impl) => {
                 processed_items.extend(process_item_impl(item_impl, &machine_types)?);
@@ -55,10 +54,10 @@ pub(crate) fn create_abstract_machine(
             _ => panic!("Unexpected item type"),
         }
     }
-    abstract_machine.items = processed_items;
+    abstract_description.items = processed_items;
 
     // add field-manipulate to items
-    manipulate::apply_to_items(&mut abstract_machine.items, ManipulateKind::Abstr);
+    manipulate::apply_to_items(&mut abstract_description.items, ManipulateKind::Abstr);
 
-    Ok(abstract_machine)
+    Ok(abstract_description)
 }

core/machine-check-machine/src/description.rs
@@ -14,11 +14,11 @@ use syn::Item;
 
 use crate::{wir::IntoSyn, MachineDescription, MachineErrors};
 
-pub fn create_ssa_machine(items: Vec<Item>) -> Result<MachineDescription, MachineErrors> {
-    create_ssa_machine_inner(items).map_err(DescriptionErrors::convert_inner)
+pub fn create_description(items: Vec<Item>) -> Result<MachineDescription, MachineErrors> {
+    create_description_inner(items).map_err(DescriptionErrors::convert_inner)
 }
 
-pub(crate) fn create_ssa_machine_inner(
+pub(crate) fn create_description_inner(
     mut items: Vec<Item>,
 ) -> Result<MachineDescription, DescriptionErrors> {
     let mut macro_expander = expand_macros::MacroExpander::new();

core/machine-check-machine/src/refin.rs
@@ -16,7 +16,7 @@ mod item_struct;
 mod rules;
 mod util;
 
-pub(crate) fn create_refinement_machine(
+pub(crate) fn create_refinement_description(
     abstract_machine: &MachineDescription,
 ) -> Result<MachineDescription, BackwardError> {
     // create items to add to the module

commit 3ddbf0344846085e5cf28ca09ab2eaefc940e4aa

Fix non-working write_machine feature in machine-check-machine
I mistakenly broke this today in 7630b1523d6cbbb5b5d0efaa5b00f3341c77344f.

core/machine-check-machine/src/lib.rs
@@ -98,7 +98,7 @@ fn process_items(items: &mut Vec<Item>) -> Result<(), Errors> {
 
     #[cfg(feature = "write_machine")]
     if let Some(out_dir) = &out_dir {
-        std::fs::write(out_dir.join("machine_ssa.rs"), unparse(&ssa_machine))
+        std::fs::write(out_dir.join("description.rs"), unparse(&description))
             .expect("SSA machine file should be writable");
     }
 
@@ -106,8 +106,11 @@ fn process_items(items: &mut Vec<Item>) -> Result<(), Errors> {
 
     #[cfg(feature = "write_machine")]
     if let Some(out_dir) = &out_dir {
-        std::fs::write(out_dir.join("machine_abstr.rs"), unparse(&abstract_machine))
-            .expect("Abstract machine file should be writable");
+        std::fs::write(
+            out_dir.join("description_abstr.rs"),
+            unparse(&abstract_description),
+        )
+        .expect("Abstract machine file should be writable");
     }
 
     let refinement_description =
@@ -129,10 +132,10 @@ fn process_items(items: &mut Vec<Item>) -> Result<(), Errors> {
     #[cfg(feature = "write_machine")]
     if let Some(out_dir) = &out_dir {
         std::fs::write(
-            out_dir.join("machine_full.rs"),
+            out_dir.join("description_full.rs"),
             unparse(&Description {
                 items: items.clone(),
-                panic_messages: ssa_machine.panic_messages.clone(),
+                panic_messages: description.panic_messages.clone(),
             }),
         )
         .expect("Full machine file should be writable");