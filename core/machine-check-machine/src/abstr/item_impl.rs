151aa1fbd5a7b1c9d3f21490fafb4a43fa466cf4
Jan Onderka <onderjan@fit.cvut.cz>
2025-05-22 15:29:51 +0200

    Subsume abstract description impl_item_fn into parent module
    Both are now nicer and shorter, so we can happily combine them.

--- core/machine-check-machine/src/abstr/item_impl.rs
+++ core/machine-check-machine/src/abstr/item_impl.rs
@@ -1,13 +1,12 @@
-mod impl_item_fn;
-
 use crate::{
-    wir::{WIdent, WItemImpl, WItemImplTrait, WPath, WPathSegment, YConverted},
+    abstr::{WAbstrItemImplTrait, YAbstr, ZAbstr, ZAbstrIfPolarity},
+    wir::{
+        WBlock, WExpr, WExprCall, WIdent, WIfCondition, WIfConditionIdent, WImplItemFn, WItemImpl,
+        WItemImplTrait, WPath, WPathSegment, WPhiMaybeTaken, WSignature, WStmt, WStmtAssign,
+        WStmtIf, YConverted, ZConverted,
+    },
     Error,
 };
-
-use self::impl_item_fn::fold_impl_item_fn;
-
-use super::{WAbstrItemImplTrait, YAbstr};
 
 pub fn preprocess_item_impl(item_impl: &WItemImpl<YConverted>) -> Result<Option<WPath>, Error> {
     let Some(WItemImplTrait::Machine) = item_impl.trait_ else {
@@ -56,4 +55,143 @@ pub fn process_item_impl(
     }
 
     Ok(results)
+}
+
+pub fn fold_impl_item_fn(
+    impl_item_fn: WImplItemFn<YConverted>,
+) -> Result<WImplItemFn<YAbstr>, Error> {
+    let signature = WSignature {
+        ident: impl_item_fn.signature.ident,
+        inputs: impl_item_fn.signature.inputs,
+        output: impl_item_fn.signature.output,
+    };
+    let block = fold_block(impl_item_fn.block);
+
+    Ok(WImplItemFn {
+        signature,
+        locals: impl_item_fn.locals,
+        block,
+        result: impl_item_fn.result,
+    })
+}
+
+fn fold_block(block: WBlock<ZConverted>) -> WBlock<ZAbstr> {
+    WBlock {
+        stmts: block.stmts.into_iter().flat_map(fold_stmt).collect(),
+    }
+}
+
+fn fold_stmt(stmt: WStmt<ZConverted>) -> Vec<WStmt<ZAbstr>> {
+    match stmt {
+        WStmt::Assign(stmt_assign) => {
+            vec![WStmt::Assign(WStmtAssign {
+                left: stmt_assign.left,
+                right: stmt_assign.right,
+            })]
+        }
+        WStmt::If(stmt_if) => fold_if(stmt_if),
+    }
+}
+
+fn fold_if(stmt_if: WStmtIf<ZConverted>) -> Vec<WStmt<ZAbstr>> {
+    let condition_ident = match stmt_if.condition {
+        WIfCondition::Ident(condition_ident) => condition_ident,
+        WIfCondition::Literal(lit) => {
+            // if statements with literal conditions can be used as-is
+            // just fold the inner blocks
+            return vec![WStmt::If(WStmtIf {
+                condition: WIfCondition::Literal(lit),
+                then_block: fold_block(stmt_if.then_block),
+                else_block: fold_block(stmt_if.else_block),
+            })];
+        }
+    };
+
+    // split into two if statements with then branch for each branch of original:
+    // 1. can be true
+    // 2. can be false
+    // in then branch, retain Taken within the statements, but eliminate NotTaken
+    // in else branch, convert the Taken from then branch to NotTaken
+
+    let can_be_true_stmt_if = create_branch_if(&condition_ident.ident, true, stmt_if.then_block);
+    let can_be_false_stmt_if = create_branch_if(&condition_ident.ident, false, stmt_if.else_block);
+
+    vec![
+        WStmt::If(can_be_true_stmt_if),
+        WStmt::If(can_be_false_stmt_if),
+    ]
+}
+
+fn create_branch_if(
+    condition: &WIdent,
+    polarity: bool,
+    taken_block: WBlock<ZConverted>,
+) -> WStmtIf<ZAbstr> {
+    let (taken_block, not_taken_block) = process_taken_branch_block(condition, taken_block);
+
+    WStmtIf {
+        condition: WIfCondition::Ident(WIfConditionIdent {
+            polarity: ZAbstrIfPolarity(polarity),
+            ident: condition.clone(),
+        }),
+        then_block: taken_block,
+        else_block: not_taken_block,
+    }
+}
+
+fn process_taken_branch_block(
+    condition: &WIdent,
+    taken_block: WBlock<ZConverted>,
+) -> (WBlock<ZAbstr>, WBlock<ZAbstr>) {
+    // change Taken statements to MaybeTaken and also add them changed to NotTaken to else block
+    // eliminate the NotTaken statements
+    let mut taken_stmts = Vec::new();
+    let mut not_taken_stmts = Vec::new();
+
+    for stmt in taken_block.stmts {
+        let stmt_assign = match stmt {
+            WStmt::Assign(stmt_assign) => stmt_assign,
+            WStmt::If(stmt_if) => {
+                taken_stmts.extend(fold_if(stmt_if));
+                continue;
+            }
+        };
+
+        let taken_ident = match stmt_assign.right {
+            WExpr::Call(WExprCall::PhiTaken(ident)) => ident,
+            WExpr::Call(WExprCall::PhiNotTaken) => {
+                // eliminate NotTaken, do not retain the statement
+                continue;
+            }
+            _ => {
+                // does not concern itself with phi taken / not taken
+                // fold and retain statement in taken
+                taken_stmts.extend(fold_stmt(WStmt::Assign(stmt_assign)));
+                continue;
+            }
+        };
+
+        // this was Taken
+        // retain as MaybeTaken
+        taken_stmts.push(WStmt::Assign(WStmtAssign {
+            left: stmt_assign.left.clone(),
+            right: WExpr::Call(WExprCall::PhiMaybeTaken(WPhiMaybeTaken {
+                taken: taken_ident,
+                condition: condition.clone(),
+            })),
+        }));
+
+        // also add as NotTaken to the else block
+        not_taken_stmts.push(WStmt::Assign(WStmtAssign {
+            left: stmt_assign.left,
+            right: WExpr::Call(WExprCall::PhiNotTaken),
+        }));
+    }
+
+    (
+        WBlock { stmts: taken_stmts },
+        WBlock {
+            stmts: not_taken_stmts,
+        },
+    )
 }
