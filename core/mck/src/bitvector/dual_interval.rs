73b764dc44f5d8723f5aeb7a5157dbbc74e1a461
Jan Onderka <onderjan@fit.cvut.cz>
2025-05-24 22:46:40 +0200

    Start working on arithmetic ops for dual-interval domain
    The idea is to use the appropriate interpretation of the intervals for each operation: wrapping (esp. for add/sub/mul), unsigned, or signed. Then, for standard (non-comparison) operations, the result will be converted to the dual-interval domain.

--- core/mck/src/bitvector/dual_interval.rs
+++ core/mck/src/bitvector/dual_interval.rs
@@ -1,23 +1,104 @@
+use num::{
+    traits::{WrappingAdd, WrappingMul, WrappingNeg, WrappingSub},
+    PrimInt,
+};
+
+mod arith;
+
+trait UnsignedPrimitive: PrimInt + WrappingAdd + WrappingSub + WrappingMul + WrappingNeg {
+    type Signed: SignedPrimitive;
+
+    const ZERO: Self;
+    const UNDERHALF: Self;
+    const OVERHALF: Self;
+    const MAX: Self;
+
+    fn cast_signed(self) -> Self::Signed;
+}
+trait SignedPrimitive: PrimInt + WrappingAdd + WrappingSub + WrappingMul + WrappingNeg {
+    type Unsigned: UnsignedPrimitive;
+    fn cast_unsigned(self) -> Self::Unsigned;
+}
+
 /// An interval, with a minimum and a maximum value.
 ///
 /// It is guaranteed that min <= max, which means the interval
 /// does not support wrapping nor representing an empty set.
-#[derive(Clone, Copy, Hash)]
-pub(crate) struct NonWrappingInterval<T: Ord + Clone + Copy> {
+#[derive(Clone, Copy, Hash, PartialEq, Eq)]
+struct NonWrappingInterval<T: Ord + Clone + Copy> {
     min: T,
     max: T,
 }
 
 impl<T: Ord + Clone + Copy> NonWrappingInterval<T> {
-    pub fn from_value(value: T) -> Self {
+    fn from_value(value: T) -> Self {
         Self {
             min: value,
             max: value,
         }
     }
 
-    pub fn contains_value(self, other: T) -> bool {
+    fn contains_value(self, other: T) -> bool {
         self.min <= other && other <= self.max
+    }
+
+    fn union(self, other: Self) -> Self {
+        Self {
+            min: self.min.min(other.min),
+            max: self.max.max(other.max),
+        }
+    }
+
+    fn union_opt(a: Option<Self>, b: Option<Self>) -> Option<Self> {
+        match (a, b) {
+            (None, None) => None,
+            (None, Some(b)) => Some(b),
+            (Some(a), None) => Some(a),
+            (Some(a), Some(b)) => Some(a.union(b)),
+        }
+    }
+}
+
+impl<U: UnsignedPrimitive> NonWrappingInterval<U> {
+    fn into_wrapping(self) -> WrappingInterval<U> {
+        WrappingInterval {
+            start: self.min,
+            end: self.max,
+        }
+    }
+}
+
+/// A wrapping interval.
+///
+/// If start <= end (unsigned), the interval represents [start,end].
+/// If start > end, the interval represents the union of [T_MIN, end] and [start, T_MAX].
+#[derive(Clone, Copy, Hash, PartialEq, Eq)]
+struct WrappingInterval<U: UnsignedPrimitive> {
+    start: U,
+    end: U,
+}
+
+impl<U: UnsignedPrimitive> WrappingInterval<U> {
+    fn from_value(value: U) -> Self {
+        Self {
+            start: value,
+            end: value,
+        }
+    }
+
+    fn full() -> Self {
+        Self {
+            start: U::ZERO,
+            end: U::MAX,
+        }
+    }
+
+    fn contains_value(self, value: U) -> bool {
+        if self.start <= self.end {
+            self.start <= value && value <= self.end
+        } else {
+            value <= self.end || self.start <= value
+        }
     }
 }
 
@@ -43,30 +124,147 @@ impl<T: Ord + Clone + Copy> NonWrappingInterval<T> {
 /// their product (in the meaning of abstract interpretation). We pay for the
 /// increased precision compared to a standard interval domain (which forms a lattice)
 /// or a wrapping-interval domain (which does not, but can be more precise than
-/// a non-wrapping interval) by a 2x increase in (rough) time and memory, which
-/// should not be problematic for most uses.
-#[derive(Clone, Copy, Hash)]
-pub(crate) struct DualInterval<T: Ord + Clone + Copy> {
+/// a non-wrapping interval) by an increase in time and memory, which should not
+/// be problematic for our use.
+#[derive(Clone, Copy, Hash, PartialEq, Eq)]
+pub(crate) struct DualInterval<U: UnsignedPrimitive> {
     // The interval usually located between (including) 0 and (2^N)/2-1.
     //
     // If it is not, it must be equal to the far half.
-    near_half: NonWrappingInterval<T>,
+    near_half: NonWrappingInterval<U>,
     // The interval usually located between (including) (2^N)/2 and (2^N)-1.
     //
     // If it is not, it must be equal to the near half.
-    far_half: NonWrappingInterval<T>,
+    far_half: NonWrappingInterval<U>,
 }
 
-#[allow(dead_code)]
-impl<T: Ord + Clone + Copy> DualInterval<T> {
-    pub fn from_value(value: T) -> Self {
+impl<U: UnsignedPrimitive> DualInterval<U> {
+    pub fn from_value(value: U) -> Self {
         Self {
             near_half: NonWrappingInterval::from_value(value),
             far_half: NonWrappingInterval::from_value(value),
         }
     }
 
-    pub fn contains_value(self, value: T) -> bool {
+    pub fn contains_value(self, value: U) -> bool {
         self.near_half.contains_value(value) || self.far_half.contains_value(value)
+    }
+
+    pub fn full() -> Self {
+        Self {
+            near_half: NonWrappingInterval {
+                min: U::ZERO,
+                max: U::UNDERHALF,
+            },
+            far_half: NonWrappingInterval {
+                min: U::OVERHALF,
+                max: U::MAX,
+            },
+        }
+    }
+
+    fn from_wrapping_interval(a: WrappingInterval<U>) -> Self {
+        let (near_half, far_half) = opt_halves(a);
+        Self::from_opt_halves(near_half, far_half)
+    }
+
+    fn from_wrapping_intervals(intervals: &[WrappingInterval<U>]) -> Self {
+        let mut near_half = None;
+        let mut far_half = None;
+
+        for interval in intervals {
+            let (interval_near_half, interval_far_half) = opt_halves(*interval);
+            near_half = NonWrappingInterval::union_opt(near_half, interval_near_half);
+            far_half = NonWrappingInterval::union_opt(far_half, interval_far_half);
+        }
+
+        Self::from_opt_halves(near_half, far_half)
+    }
+
+    fn from_opt_halves(
+        near_half: Option<NonWrappingInterval<U>>,
+        far_half: Option<NonWrappingInterval<U>>,
+    ) -> Self {
+        let near_half = near_half.unwrap_or(NonWrappingInterval {
+            min: U::ZERO,
+            max: U::UNDERHALF,
+        });
+        let far_half = far_half.unwrap_or(NonWrappingInterval {
+            min: U::OVERHALF,
+            max: U::MAX,
+        });
+        Self {
+            near_half,
+            far_half,
+        }
+    }
+}
+
+fn opt_halves<U: UnsignedPrimitive>(
+    a: WrappingInterval<U>,
+) -> (
+    Option<NonWrappingInterval<U>>,
+    Option<NonWrappingInterval<U>>,
+) {
+    let preserves_unsigned_seam = a.start <= a.end;
+    let preserves_signed_seam = a.start >= U::OVERHALF || a.end < U::OVERHALF;
+
+    // We need to split the wrapping interval to two if it crosses a signed
+    // or unsigned seam.
+    match (preserves_unsigned_seam, preserves_signed_seam) {
+        (true, true) => {
+            // Preserves both seams.
+            // Just return the interval in the given half.
+            let not_crossing = NonWrappingInterval {
+                min: a.start,
+                max: a.end,
+            };
+            if a.end <= U::OVERHALF {
+                (Some(not_crossing), None)
+            } else {
+                (None, Some(not_crossing))
+            }
+        }
+        (true, false) => {
+            // Preserves the unsigned seam, but crosses the signed seam.
+            (
+                Some(NonWrappingInterval {
+                    min: a.start,
+                    max: U::UNDERHALF,
+                }),
+                Some(NonWrappingInterval {
+                    min: U::OVERHALF,
+                    max: a.end,
+                }),
+            )
+        }
+        (false, true) => {
+            // Crosses the unsigned seam, but preserves the signed seam.
+            (
+                Some(NonWrappingInterval {
+                    min: U::ZERO,
+                    max: a.end,
+                }),
+                Some(NonWrappingInterval {
+                    min: a.start,
+                    max: U::MAX,
+                }),
+            )
+        }
+        (false, false) => {
+            // Crosses both seams. This means that both halves must contain
+            // the values at both ends. We can only represent this
+            // in the dual-interval domain using the full interval.
+            (
+                Some(NonWrappingInterval {
+                    min: U::ZERO,
+                    max: U::UNDERHALF,
+                }),
+                Some(NonWrappingInterval {
+                    min: U::OVERHALF,
+                    max: U::MAX,
+                }),
+            )
+        }
     }
 }
---
+++ core/mck/src/bitvector/dual_interval/arith.rs
@@ -0,0 +1,128 @@
+use crate::forward::HwArith;
+
+use super::{DualInterval, UnsignedPrimitive, WrappingInterval};
+
+impl<U: UnsignedPrimitive> WrappingInterval<U> {
+    fn hw_add(self, rhs: Self) -> Self {
+        // ensure the produced bounds are less than 2^L apart, produce a full interval otherwise
+        if self.is_addsub_full(rhs) {
+            Self::full()
+        } else {
+            // wrapping and fully monotonic: add bounds
+            let start = self.start.add(rhs.start);
+            let end = self.end.add(rhs.end);
+
+            Self { start, end }
+        }
+    }
+
+    fn hw_sub(self, rhs: Self) -> Self {
+        // ensure the produced bounds are less than 2^L apart, produce a full interval otherwise
+        if self.is_addsub_full(rhs) {
+            Self::full()
+        } else {
+            // wrapping, monotonic on lhs, anti-monotonic on rhs: subtract bounds, remember to flip rhs bounds
+            let start = self.start.sub(rhs.end);
+            let end = self.end.sub(rhs.start);
+
+            Self { start, end }
+        }
+    }
+
+    fn hw_mul(self, rhs: Self) -> Self {
+        let lhs_start = self.start;
+        let rhs_start = rhs.start;
+        let start = lhs_start.wrapping_mul(&rhs_start);
+        let lhs_diff = self.bound_diff();
+        let rhs_diff = rhs.bound_diff();
+
+        let Some(diff_product) = lhs_diff.checked_mul(&rhs_diff) else {
+            return Self::full();
+        };
+        let Some(diff_start_product) = lhs_diff.checked_mul(&rhs_start) else {
+            return Self::full();
+        };
+        let Some(start_diff_product) = lhs_start.checked_mul(&rhs_diff) else {
+            return Self::full();
+        };
+        let Some(result_len) = diff_product
+            .checked_add(&diff_start_product)
+            .and_then(|v| v.checked_add(&start_diff_product))
+        else {
+            return Self::full();
+        };
+
+        let end = start.wrapping_add(&result_len);
+
+        Self { start, end }
+    }
+
+    fn is_addsub_full(self, rhs: Self) -> bool {
+        let lhs_diff = self.bound_diff();
+        let rhs_diff = rhs.bound_diff();
+
+        let wrapped_total_len = lhs_diff.wrapping_add(&rhs_diff);
+        wrapped_total_len < lhs_diff || wrapped_total_len < rhs_diff
+    }
+
+    pub fn bound_diff(&self) -> U {
+        self.end.wrapping_sub(&self.start)
+    }
+}
+
+impl<U: UnsignedPrimitive> HwArith for DualInterval<U> {
+    fn arith_neg(self) -> Self {
+        // arithmetic negation
+        // for wrapping arithmetic, arithmetic negation is same as subtracting the value from 0
+        // subtract from interval constructed from 0
+        Self::from_value(U::zero()).sub(self)
+    }
+
+    fn add(self, rhs: Self) -> Self {
+        resolve_by_wrapping(self, rhs, |a, b| a.hw_add(b))
+    }
+
+    fn sub(self, rhs: Self) -> Self {
+        resolve_by_wrapping(self, rhs, |a, b| a.hw_sub(b))
+    }
+
+    fn mul(self, rhs: Self) -> Self {
+        resolve_by_wrapping(self, rhs, |a, b| a.hw_mul(b))
+    }
+
+    fn udiv(self, rhs: Self) -> Self {
+        // TODO: compute using unsigned intervals
+        todo!()
+    }
+
+    fn sdiv(self, rhs: Self) -> Self {
+        // TODO: compute using signed intervals
+        todo!()
+    }
+
+    fn urem(self, rhs: Self) -> Self {
+        // TODO: compute using unsigned intervals
+        todo!()
+    }
+
+    fn srem(self, rhs: Self) -> Self {
+        // TODO: compute using signed intervals
+        todo!()
+    }
+}
+
+fn resolve_by_wrapping<U: UnsignedPrimitive>(
+    a: DualInterval<U>,
+    b: DualInterval<U>,
+    op_fn: fn(WrappingInterval<U>, WrappingInterval<U>) -> WrappingInterval<U>,
+) -> DualInterval<U> {
+    // TODO: optimise cases where the a, b, or both can be represented by one wrapping interval
+
+    // resolve all combinations of halves separately
+    let nn_result = op_fn(a.near_half.into_wrapping(), b.near_half.into_wrapping());
+    let nf_result = op_fn(a.near_half.into_wrapping(), b.far_half.into_wrapping());
+    let fn_result = op_fn(a.far_half.into_wrapping(), b.near_half.into_wrapping());
+    let ff_result = op_fn(a.far_half.into_wrapping(), b.far_half.into_wrapping());
+
+    DualInterval::from_wrapping_intervals(&[nn_result, nf_result, fn_result, ff_result])
+}
