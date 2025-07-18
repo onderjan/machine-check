00c4cf3dd33367b35a68aa6a8b66824ad07c781d
Jan Onderka <onderjan@fit.cvut.cz>
2025-07-18 20:57:59 +0200

    Split up fixed-point computation iterations and conclusion

--- core/machine-check-exec/src/model_check/property_checker/labelling_computer/fixed_point.rs
+++ core/machine-check-exec/src/model_check/property_checker/labelling_computer/fixed_point.rs
@@ -1,14 +1,24 @@
 use std::collections::{BTreeMap, BTreeSet};
 
 use log::{log_enabled, trace};
-use machine_check_common::{property::FixedPointOperator, ExecError, ThreeValued};
+use machine_check_common::{property::FixedPointOperator, ExecError, StateId, ThreeValued};
 
 use crate::{
     model_check::property_checker::{
-        labelling_computer::LabellingComputer, CacheEntry, CheckValue,
+        labelling_computer::LabellingComputer, CacheEntry, CheckValue, FixedPointHistory,
     },
     FullMachine,
 };
+
+struct FixedPointIterationParams<'a> {
+    subproperty_index: usize,
+    op: &'a FixedPointOperator,
+
+    old_history: Option<FixedPointHistory>,
+    old_values: BTreeMap<StateId, CheckValue>,
+    update: BTreeMap<StateId, CheckValue>,
+    all_updated: BTreeSet<StateId>,
+}
 
 impl<M: FullMachine> LabellingComputer<'_, M> {
     pub fn compute_fixed_point_op(
@@ -53,7 +63,7 @@ impl<M: FullMachine> LabellingComputer<'_, M> {
 
         // initialise fixed-point computation labelling
 
-        let (old_values, mut update) = {
+        let (old_values, update) = {
             let fixed_point_computation = self
                 .computations
                 .get_mut(&subproperty_index)
@@ -79,145 +89,168 @@ impl<M: FullMachine> LabellingComputer<'_, M> {
             (old_values, update)
         };
 
-        let mut all_updated = BTreeSet::new();
+        let mut params = FixedPointIterationParams {
+            subproperty_index,
+            op,
+            old_history,
+            old_values,
+            update,
+            all_updated: BTreeSet::new(),
+        };
 
         // compute inner property labelling and update variable labelling until they match
-        loop {
-            trace!(
-                "Fixed point {:?} not reached yet, update: {:?}",
-                subproperty_index,
-                update
-            );
+        while !params.update.is_empty() {
+            self.fixed_point_iteration(&mut params)?;
+        }
 
-            // fixed point not reached yet
-            // change the values and updated
+        self.fixed_point_conclusion(&mut params)?;
 
-            let mut current_update = BTreeMap::new();
-            std::mem::swap(&mut update, &mut current_update);
+        Ok(())
+    }
 
-            {
-                let cache_entry = self.property_checker.cache.last_mut().unwrap();
-                let fixed_point_computation = self
-                    .computations
-                    .get_mut(&subproperty_index)
-                    .expect("Fixed-point operation should have a computation");
+    fn fixed_point_iteration(
+        &mut self,
+        params: &mut FixedPointIterationParams,
+    ) -> Result<(), ExecError> {
+        trace!(
+            "Fixed point {:?} not reached yet, update: {:?}",
+            params.subproperty_index,
+            params.update
+        );
 
-                for (state_id, update_value) in current_update {
-                    fixed_point_computation
-                        .values
-                        .insert(state_id, update_value.clone());
-                    fixed_point_computation.updated.insert(state_id);
-                    all_updated.insert(state_id);
-                    Self::insert_history(cache_entry, state_id, update_value);
-                }
+        // fixed point not reached yet
+        // change the values and updated
 
-                trace!(
-                    "Fixed point {:?} updated: {:?}",
-                    subproperty_index,
-                    fixed_point_computation
-                );
+        let mut current_update = BTreeMap::new();
+        std::mem::swap(&mut params.update, &mut current_update);
 
-                if log_enabled!(log::Level::Trace) {
-                    if let Some(old_history) = &old_history {
-                        for state_id in self.space.states() {
-                            let current = fixed_point_computation.values.get(&state_id);
-                            let old = old_history.states.get(&state_id).and_then(|state_history| {
-                                state_history
-                                    .range(0..=cache_entry.time_instant)
-                                    .last()
-                                    .map(|(_, value)| value)
-                            });
-                            if current != old {
-                                trace!(
-                                    "State {} mismatch from old {:?} to current {:?}",
-                                    state_id,
-                                    old,
-                                    current
-                                );
-                            }
-                        }
-                    }
-                }
-            }
+        let cache_entry = self.property_checker.cache.last_mut().unwrap();
+        let fixed_point_computation = self
+            .computations
+            .get_mut(&params.subproperty_index)
+            .expect("Fixed-point operation should have a computation");
 
-            self.compute_labelling(op.inner)?;
+        for (state_id, update_value) in current_update {
+            fixed_point_computation
+                .values
+                .insert(state_id, update_value.clone());
+            fixed_point_computation.updated.insert(state_id);
+            params.all_updated.insert(state_id);
+            Self::insert_history(cache_entry, state_id, update_value);
+        }
 
-            self.property_checker.cache.last_mut().unwrap().time_instant += 1;
+        trace!(
+            "Fixed point {:?} updated: {:?}",
+            params.subproperty_index,
+            fixed_point_computation
+        );
 
-            //println!("Updated in this iteration: {:?}", current_update);
-
-            let fixed_point_computation = Self::computation(&self.computations, subproperty_index);
-            let inner_computation = Self::computation(&self.computations, op.inner);
-
-            for state_id in inner_computation.updated.iter().copied() {
-                let fixed_point_value = fixed_point_computation.values.get(&state_id).unwrap();
-                let inner_value = inner_computation.values.get(&state_id).unwrap();
-                if fixed_point_value != inner_value {
-                    update.insert(state_id, inner_value.clone());
-                }
-            }
-
-            let inner_computation = Self::computation_mut(&mut self.computations, op.inner);
-            inner_computation.updated.clear();
-
-            if update.is_empty() {
-                // we reached the fixed point
-                // the updated values from the outside point of view are those that differ from the ones before
-                // these must have been updated at least once
-
-                let fixed_point_computation =
-                    Self::computation_mut(&mut self.computations, subproperty_index);
-
-                fixed_point_computation.updated.clear();
-
-                for state_id in all_updated {
-                    let current_value = fixed_point_computation.values.get(&state_id).unwrap();
-                    if let Some(old_value) = old_values.get(&state_id) {
-                        if old_value == current_value {
-                            continue;
-                        }
-                    }
-                    fixed_point_computation.updated.insert(state_id);
-                }
-
-                trace!(
-                    "Fixed point {:?} reached: {:?}",
-                    subproperty_index,
-                    fixed_point_computation
-                );
-
-                if log_enabled!(log::Level::Trace) {
-                    let cache_entry = self.property_checker.cache.last_mut().unwrap();
-                    if let (Some(old_history), Some(current_history)) =
-                        (&old_history, cache_entry.histories.get(&subproperty_index))
-                    {
-                        let mut unchanged_states = BTreeSet::new();
-                        let mut changed_states = BTreeSet::new();
-                        for state_id in self.space.states() {
-                            let current = current_history.states.get(&state_id);
-                            let old = old_history.states.get(&state_id);
-                            if current != old {
-                                changed_states.insert(state_id);
-                            } else {
-                                unchanged_states.insert(state_id);
-                            }
-                        }
+        if log_enabled!(log::Level::Trace) {
+            if let Some(old_history) = &params.old_history {
+                for state_id in self.space.states() {
+                    let current = fixed_point_computation.values.get(&state_id);
+                    let old = old_history.states.get(&state_id).and_then(|state_history| {
+                        state_history
+                            .range(0..=cache_entry.time_instant)
+                            .last()
+                            .map(|(_, value)| value)
+                    });
+                    if current != old {
                         trace!(
-                            "Old history: {:#?}, current history: {:#?}",
-                            old_history,
-                            current_history
-                        );
-                        trace!(
-                            "Changed states: {:?}, unchanged states: {:?}",
-                            changed_states,
-                            unchanged_states,
+                            "State {} mismatch from old {:?} to current {:?}",
+                            state_id,
+                            old,
+                            current
                         );
                     }
                 }
-
-                return Ok(());
             }
         }
+
+        self.compute_labelling(params.op.inner)?;
+
+        self.property_checker.cache.last_mut().unwrap().time_instant += 1;
+
+        //println!("Updated in this iteration: {:?}", current_update);
+
+        let fixed_point_computation =
+            Self::computation(&self.computations, params.subproperty_index);
+        let inner_computation = Self::computation(&self.computations, params.op.inner);
+
+        for state_id in inner_computation.updated.iter().copied() {
+            let fixed_point_value = fixed_point_computation.values.get(&state_id).unwrap();
+            let inner_value = inner_computation.values.get(&state_id).unwrap();
+            if fixed_point_value != inner_value {
+                params.update.insert(state_id, inner_value.clone());
+            }
+        }
+
+        let inner_computation = Self::computation_mut(&mut self.computations, params.op.inner);
+        inner_computation.updated.clear();
+
+        Ok(())
+    }
+
+    fn fixed_point_conclusion(
+        &mut self,
+        params: &mut FixedPointIterationParams,
+    ) -> Result<(), ExecError> {
+        // we reached the fixed point
+        // the updated values from the outside point of view are those that differ from the ones before
+        // these must have been updated at least once
+
+        let fixed_point_computation =
+            Self::computation_mut(&mut self.computations, params.subproperty_index);
+
+        fixed_point_computation.updated.clear();
+
+        for state_id in params.all_updated.iter().cloned() {
+            let current_value = fixed_point_computation.values.get(&state_id).unwrap();
+            if let Some(old_value) = params.old_values.get(&state_id) {
+                if old_value == current_value {
+                    continue;
+                }
+            }
+            fixed_point_computation.updated.insert(state_id);
+        }
+
+        trace!(
+            "Fixed point {:?} reached: {:?}",
+            params.subproperty_index,
+            fixed_point_computation
+        );
+
+        if log_enabled!(log::Level::Trace) {
+            let cache_entry = self.property_checker.cache.last_mut().unwrap();
+            if let (Some(old_history), Some(current_history)) = (
+                &params.old_history,
+                cache_entry.histories.get(&params.subproperty_index),
+            ) {
+                let mut unchanged_states = BTreeSet::new();
+                let mut changed_states = BTreeSet::new();
+                for state_id in self.space.states() {
+                    let current = current_history.states.get(&state_id);
+                    let old = old_history.states.get(&state_id);
+                    if current != old {
+                        changed_states.insert(state_id);
+                    } else {
+                        unchanged_states.insert(state_id);
+                    }
+                }
+                trace!(
+                    "Old history: {:#?}, current history: {:#?}",
+                    old_history,
+                    current_history
+                );
+                trace!(
+                    "Changed states: {:?}, unchanged states: {:?}",
+                    changed_states,
+                    unchanged_states,
+                );
+            }
+        }
+
+        Ok(())
     }
 
     pub fn compute_fixed_variable(
