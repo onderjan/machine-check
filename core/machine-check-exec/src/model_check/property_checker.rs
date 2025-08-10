46b9c666d59011fb2378d0069f20fd0b82f0936d
Jan Onderka <onderjan@onderjan.net>
2025-08-10 16:51:34 +0200

    (MAYBE BROKEN) Move the Focus struct to its own module
    The new Focus struct handles the dirty states and affected states that reachable from the dirty states in one time instant.

--- core/machine-check-exec/src/model_check/property_checker.rs
+++ core/machine-check-exec/src/model_check/property_checker.rs
@@ -1,3 +1,4 @@
+mod focus;
 mod history;
 mod labelling_computer;
 mod labelling_getter;
@@ -15,6 +16,7 @@ use mck::concr::FullMachine;
 
 use crate::{
     model_check::property_checker::{
+        focus::Focus,
         history::{CheckValue, FixedPointHistory, TimedCheckValue},
         labelling_computer::LabellingComputer,
     },
@@ -33,77 +35,6 @@ pub struct PropertyChecker {
     computations: Vec<FixedPointComputation>,
 
     focus: Focus,
-}
-
-#[derive(Debug, Clone)]
-
-struct Focus {
-    depth: usize,
-    dirty: BTreeSet<StateId>,
-    affected: BTreeSet<StateId>,
-}
-
-impl Focus {
-    fn new(property: &Property) -> Self {
-        let result = Self {
-            depth: property.transition_depth(),
-            dirty: BTreeSet::new(),
-            affected: BTreeSet::new(),
-        };
-        trace!("Focus depth: {}", result.depth);
-        result
-    }
-
-    fn clear(&mut self) {
-        self.dirty.clear();
-        self.affected.clear();
-    }
-
-    fn dirty(&self) -> &BTreeSet<StateId> {
-        &self.dirty
-    }
-
-    fn dirty_iter(&self) -> impl Iterator<Item = StateId> + use<'_> {
-        self.dirty().iter().copied()
-    }
-
-    fn affected(&self) -> &BTreeSet<StateId> {
-        &self.affected
-    }
-
-    fn extend_dirty<M: FullMachine>(
-        &mut self,
-        space: &StateSpace<M>,
-        iter: impl Iterator<Item = StateId>,
-    ) {
-        for state_id in iter {
-            self.insert_dirty(space, state_id);
-        }
-    }
-
-    fn insert_dirty<M: FullMachine>(&mut self, space: &StateSpace<M>, state_id: StateId) {
-        if self.dirty.contains(&state_id) {
-            // already dirty
-            return;
-        }
-
-        self.dirty.insert(state_id);
-        self.affected.insert(state_id);
-
-        let mut current_affected = BTreeSet::from([state_id]);
-        let mut next_affected = BTreeSet::new();
-
-        for _ in 0..self.depth {
-            for state_id in current_affected.iter().copied() {
-                for direct_successor_id in space.direct_successor_iter(state_id.into()) {
-                    self.affected.insert(direct_successor_id);
-                    next_affected.insert(direct_successor_id);
-                }
-            }
-            current_affected.clear();
-            current_affected.append(&mut next_affected);
-        }
-    }
 }
 
 #[derive(Debug, Clone, Default, PartialEq, Eq)]
---
+++ core/machine-check-exec/src/model_check/property_checker/focus.rs
@@ -0,0 +1,78 @@
+use std::collections::BTreeSet;
+
+use log::trace;
+use machine_check_common::{check::Property, StateId};
+
+use crate::space::StateSpace;
+use crate::FullMachine;
+
+#[derive(Debug, Clone)]
+
+pub struct Focus {
+    depth: usize,
+    dirty: BTreeSet<StateId>,
+    affected: BTreeSet<StateId>,
+}
+
+impl Focus {
+    pub fn new(property: &Property) -> Self {
+        let result = Self {
+            depth: property.transition_depth(),
+            dirty: BTreeSet::new(),
+            affected: BTreeSet::new(),
+        };
+        trace!("Focus depth: {}", result.depth);
+        result
+    }
+
+    pub fn clear(&mut self) {
+        self.dirty.clear();
+        self.affected.clear();
+    }
+
+    pub fn dirty(&self) -> &BTreeSet<StateId> {
+        &self.dirty
+    }
+
+    pub fn dirty_iter(&self) -> impl Iterator<Item = StateId> + use<'_> {
+        self.dirty().iter().copied()
+    }
+
+    pub fn affected(&self) -> &BTreeSet<StateId> {
+        &self.affected
+    }
+
+    pub fn extend_dirty<M: FullMachine>(
+        &mut self,
+        space: &StateSpace<M>,
+        iter: impl Iterator<Item = StateId>,
+    ) {
+        for state_id in iter {
+            self.insert_dirty(space, state_id);
+        }
+    }
+
+    pub fn insert_dirty<M: FullMachine>(&mut self, space: &StateSpace<M>, state_id: StateId) {
+        if self.dirty.contains(&state_id) {
+            // already dirty
+            return;
+        }
+
+        self.dirty.insert(state_id);
+        self.affected.insert(state_id);
+
+        let mut current_affected = BTreeSet::from([state_id]);
+        let mut next_affected = BTreeSet::new();
+
+        for _ in 0..self.depth {
+            for state_id in current_affected.iter().copied() {
+                for direct_successor_id in space.direct_successor_iter(state_id.into()) {
+                    self.affected.insert(direct_successor_id);
+                    next_affected.insert(direct_successor_id);
+                }
+            }
+            current_affected.clear();
+            current_affected.append(&mut next_affected);
+        }
+    }
+}
