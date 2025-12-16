ff2c5c2f956828af4aa031a47191dd4341dd05b7
Jan Onderka <onderjan@onderjan.net>
2025-12-16 14:05:41 +0100

    Simplify the exec builder
    The arg parsing can actually be done outside the builder.

--- core/machine-check/src/builder.rs
+++ core/machine-check/src/builder.rs
@@ -1,35 +1,20 @@
-use crate::{args::ProgramArgs, ExecArgs, FullMachine};
-use clap::Parser;
+use crate::{ExecArgs, FullMachine};
 use machine_check_common::{ExecResult, PropertyMacroFn, PropertyMacros};
-use std::{collections::HashMap, marker::PhantomData};
+use std::collections::HashMap;
 
 type CreatorFn<M, D, E, A> = dyn Fn(A) -> Result<(M, D), E>;
 
 pub struct ExecBuilder<M: FullMachine, D: Send + 'static, E: 'static, A> {
     creator_fn: Box<CreatorFn<M, D, E, A>>,
-    args_parser: Box<dyn ArgsParser<A>>,
     property_macros: HashMap<String, PropertyMacroFn<D>>,
-}
-
-impl<M: FullMachine, D: Send + 'static, E: 'static> ExecBuilder<M, D, E, ()> {
-    pub fn new_basic(creator_fn: fn() -> Result<(M, D), E>) -> Self {
-        Self {
-            creator_fn: Box::new(move |()| (creator_fn)()),
-            args_parser: Box::new(BasicArgsParser),
-            property_macros: HashMap::new(),
-        }
-    }
 }
 
 impl<M: FullMachine, D: Send + 'static, E: 'static, A: clap::Args + 'static>
     ExecBuilder<M, D, E, A>
 {
-    pub fn new_with_clap_args(creator_fn: fn(A) -> Result<(M, D), E>) -> Self {
+    pub fn new(creator_fn: fn(A) -> Result<(M, D), E>) -> Self {
         Self {
             creator_fn: Box::new(creator_fn),
-            args_parser: Box::new(ClapArgsParser {
-                _phantom: PhantomData,
-            }),
             property_macros: HashMap::new(),
         }
     }
@@ -48,9 +33,7 @@ impl<M: FullMachine, D: Send + 'static, E: 'static, A> ExecBuilder<M, D, E, A> {
         self
     }
 
-    pub fn execute(self, mut args: impl Iterator<Item = String>) -> Result<ExecResult, E> {
-        // TODO: use macros
-        let (exec_args, system_args) = self.args_parser.parse_args(&mut args);
+    pub fn execute(self, exec_args: ExecArgs, system_args: A) -> Result<ExecResult, E> {
         super::setup_logging(&exec_args);
         match (self.creator_fn)(system_args) {
             Ok((system, data)) => Ok(super::execute_inner(
@@ -63,29 +46,5 @@ impl<M: FullMachine, D: Send + 'static, E: 'static, A> ExecBuilder<M, D, E, A> {
             )),
             Err(err) => Err(err),
         }
-    }
-}
-
-trait ArgsParser<A> {
-    fn parse_args(&self, args: &mut dyn Iterator<Item = String>) -> (ExecArgs, A);
-}
-
-struct BasicArgsParser;
-
-impl ArgsParser<()> for BasicArgsParser {
-    fn parse_args(&self, args: &mut dyn Iterator<Item = String>) -> (ExecArgs, ()) {
-        (ExecArgs::parse_from(args), ())
-    }
-}
-
-struct ClapArgsParser<A: clap::Args> {
-    _phantom: PhantomData<A>,
-}
-
-impl<A: clap::Args> ArgsParser<A> for ClapArgsParser<A> {
-    fn parse_args(&self, args: &mut dyn Iterator<Item = String>) -> (ExecArgs, A) {
-        let program_args = <ProgramArgs<A> as clap::Parser>::parse_from(args);
-
-        (program_args.run_args, program_args.system_args)
     }
 }
--- systems/machine-check-riscv/src/lib.rs
+++ systems/machine-check-riscv/src/lib.rs
@@ -33,7 +33,7 @@ pub fn execute(args: impl Iterator<Item = String>) -> ExecResult {
 
     todo!()*/
 
-    let builder = machine_check::ExecBuilder::new_with_clap_args(
+    let builder = machine_check::ExecBuilder::new(
         |system_args: SystemArgs| -> Result<(system::R9A02G021, Symbols), anyhow::Error> {
             let (system, symbols) = elf::parse_elf(&system_args.elf_file)?;
 
@@ -45,7 +45,9 @@ pub fn execute(args: impl Iterator<Item = String>) -> ExecResult {
         .property_macro(String::from("pc_at_symbol"), macros::pc_at_symbol)
         .property_macro(String::from("pc_within_symbol"), macros::pc_within_symbol)
         .property_macro(String::from("typed_symbol"), macros::typed_symbol);
-    match builder.execute(args) {
+
+    let (exec_args, system_args) = machine_check::parse_args(args);
+    match builder.execute(exec_args, system_args) {
         Ok(ok) => ok,
         Err(err) => {
             let err = err.to_string();
