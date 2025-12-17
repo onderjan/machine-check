bcf4963ccafb1a71bd73cd66704b697d14bfb601
Jan <onderjan@onderjan.net>
2025-12-17 13:34:01 +0100

    Comment DWARF parsing in RISC-V crate

--- systems/machine-check-riscv/src/dwarf.rs
+++ systems/machine-check-riscv/src/dwarf.rs
@@ -6,16 +6,19 @@ use gimli::{
 };
 use object::{read::elf::ElfFile32, LittleEndian, Object, ObjectSection};
 
+/// A map of symbols.
 pub struct Symbols {
     inner: HashMap<String, Symbol>,
 }
 
+/// A symbol that contains an address and byte size.
 #[derive(Debug)]
 pub struct TypedSymbol {
     pub address: u32,
     pub byte_size: u32,
 }
 
+/// A debug symbol.
 #[derive(Debug)]
 pub enum Symbol {
     Unresolved,
@@ -26,7 +29,9 @@ pub enum Symbol {
 }
 
 impl Symbols {
+    /// Produces symbols from an ELF file.
     pub fn from_elf_file(elf_file: &ElfFile32<LittleEndian>) -> anyhow::Result<Symbols> {
+        // get the DWARF from the ELF file
         let dwarf_sections = gimli::DwarfSections::load(|id| {
             elf_file
                 .section_by_name(id.name())
@@ -38,12 +43,15 @@ impl Symbols {
         let mut symbol_map = HashMap::new();
 
         let mut unit_iter = dwarf.units();
+        // iterate over DWARF units
         while let Some(header) = unit_iter.next()? {
             let unit = dwarf.unit(header)?;
             let unit_ref = unit.unit_ref(&dwarf);
 
+            // iterate over unit entries
             let mut entries_cursor = unit.entries();
             while let Some((_index, entry)) = entries_cursor.next_dfs()? {
+                // skip entries that do not have a string name
                 let Some(name_attr) = entry.attr(DwAt(0x03))? else {
                     continue;
                 };
@@ -54,10 +62,12 @@ impl Symbols {
                     continue;
                 };
 
-                let symbol = load_entry_symbol(&dwarf, &unit_ref, entry)?;
+                // process the symbol
+                let symbol = entry_symbol(&dwarf, &unit_ref, entry)?;
 
+                // try to insert the symbols
                 if symbol_map.insert(name.to_string(), symbol).is_some() {
-                    // multiple symbols with the same name
+                    // insert that there are multiple symbols with the same name
                     symbol_map.insert(name.to_string(), Symbol::Multiple);
                 }
             }
@@ -75,30 +85,42 @@ impl Symbols {
     }
 }
 
-fn load_entry_symbol<'a, R: Reader>(
+fn entry_symbol<'a, R: Reader>(
     dwarf: &'a Dwarf<R>,
     unit_ref: &UnitRef<'a, R>,
     entry: &DebuggingInformationEntry<'a, 'a, R>,
 ) -> anyhow::Result<Symbol> {
+    // see if it is typed first
     if let Some(symbol) = evaluate_typed(dwarf, unit_ref, entry)? {
         return Ok(symbol);
     }
 
+    // try to see if it has a program counter address/range
     // DW_AT_low_pc
-    if let Some(low_pc) = evaluate_attribute_address(dwarf, unit_ref, entry, 0x11, None)? {
-        if let Some(high_pc) =
-            evaluate_attribute_address(dwarf, unit_ref, entry, 0x12, Some(low_pc))?
-        {
-            return Ok(Symbol::ProgramCounterRange(Range {
-                start: low_pc,
-                end: high_pc,
-            }));
+    if let AddressEval::Address(low_pc) =
+        evaluate_attribute_address(dwarf, unit_ref, entry, 0x11, None)?
+    {
+        // DW_AT_high_pc
+        match evaluate_attribute_address(dwarf, unit_ref, entry, 0x12, Some(low_pc))? {
+            AddressEval::Absent => {
+                // PC address, definitely only has low_pc and not high_pc
+                Ok(Symbol::ProgramCounterAddress(low_pc))
+            }
+            AddressEval::EvalFailed => {
+                // PC range, but evaluation failed
+                Ok(Symbol::Unresolved)
+            }
+            AddressEval::Address(high_pc) => {
+                // PC range
+                Ok(Symbol::ProgramCounterRange(Range {
+                    start: low_pc,
+                    end: high_pc,
+                }))
+            }
         }
-
-        return Ok(Symbol::ProgramCounterAddress(low_pc));
+    } else {
+        Ok(Symbol::Unresolved)
     }
-
-    Ok(Symbol::Unresolved)
 }
 
 fn evaluate_typed<'a, R: Reader>(
@@ -106,11 +128,15 @@ fn evaluate_typed<'a, R: Reader>(
     unit_ref: &UnitRef<'a, R>,
     entry: &DebuggingInformationEntry<'a, 'a, R>,
 ) -> anyhow::Result<Option<Symbol>> {
-    // DW_AT_location
-    let Some(location) = evaluate_attribute_address(dwarf, unit_ref, entry, 0x02, None)? else {
-        return Ok(None);
+    // try to get an address from DW_AT_location
+    let location = match evaluate_attribute_address(dwarf, unit_ref, entry, 0x02, None)? {
+        AddressEval::Absent => return Ok(None),
+        AddressEval::EvalFailed => return Ok(Some(Symbol::Unresolved)),
+        AddressEval::Address(location) => location,
     };
 
+    // it may be necessary to go into types recursively before finding the true base
+    // store the unit offset; if none, the argument unit_ref will be used
     let mut unit_offset = None;
 
     loop {
@@ -121,9 +147,10 @@ fn evaluate_typed<'a, R: Reader>(
             Cow::Borrowed(entry)
         };
 
-        // DW_AT_byte_size
+        // try to get DW_AT_byte_size
         if let Some(byte_size) = current_entry.attr(DwAt(0x0b))? {
             if let Some(byte_size) = byte_size.udata_value() {
+                // we have found a valid byte size, create the typed symbol
                 return Ok(Some(Symbol::Typed(TypedSymbol {
                     address: location,
                     byte_size: byte_size as u32,
@@ -131,17 +158,25 @@ fn evaluate_typed<'a, R: Reader>(
             }
         };
 
-        // DW_AT_type
+        // look at whether this has a DW_AT_type
         let Some(ty) = current_entry.attr(DwAt(0x49))? else {
-            return Ok(None);
+            // we have an address, but we have not found a byte size
+            return Ok(Some(Symbol::Unresolved));
         };
 
         let AttributeValue::UnitRef(offset) = ty.value() else {
-            return Ok(None);
+            // we have an address, but we have not found a byte size
+            return Ok(Some(Symbol::Unresolved));
         };
 
         unit_offset = Some(offset);
     }
+}
+
+enum AddressEval {
+    Absent,
+    EvalFailed,
+    Address(u32),
 }
 
 fn evaluate_attribute_address<'a, R: Reader>(
@@ -150,26 +185,31 @@ fn evaluate_attribute_address<'a, R: Reader>(
     entry: &DebuggingInformationEntry<'a, 'a, R>,
     attribute: u16,
     base: Option<u32>,
-) -> anyhow::Result<Option<u32>> {
+) -> anyhow::Result<AddressEval> {
     let Some(attr) = entry.attr(DwAt(attribute))? else {
-        return Ok(None);
+        // the attribute is not present here
+        return Ok(AddressEval::Absent);
     };
 
     if let Some(base) = base {
         if let Some(udata) = attr.udata_value() {
-            return Ok(Some(base + udata as u32));
+            // the udata gives an offset from the base
+            return Ok(AddressEval::Address(base + udata as u32));
         }
     }
 
     if let gimli::AttributeValue::DebugAddrIndex(debug_addr_index) = attr.value() {
+        // we immediately obtain an address
         let index = dwarf.address(unit_ref, debug_addr_index)?;
-        return Ok(Some(index as u32));
+        return Ok(AddressEval::Address(index as u32));
     }
 
     let Some(expression) = attr.exprloc_value() else {
-        return Ok(None);
+        // attribute is present, but we could not figure out the address
+        return Ok(AddressEval::EvalFailed);
     };
 
+    // evaluate the DWARF expression
     let mut evaluation = expression.evaluation(unit_ref.encoding());
 
     loop {
@@ -177,15 +217,17 @@ fn evaluate_attribute_address<'a, R: Reader>(
 
         match eval_progress {
             EvaluationResult::Complete => {
+                // evaluation is complete
                 break;
             }
             EvaluationResult::RequiresIndexedAddress { index, relocate: _ } => {
+                // give the indexed address and continue
                 let index = dwarf.address(unit_ref, index)?;
                 evaluation.resume_with_indexed_address(index)?;
             }
             _ => {
-                // do not resolve anymore
-                return Ok(None);
+                // we could not figure out the address
+                return Ok(AddressEval::EvalFailed);
             }
         }
     }
@@ -193,13 +235,15 @@ fn evaluate_attribute_address<'a, R: Reader>(
     let eval_result = evaluation.result();
 
     if eval_result.len() != 1 {
-        return Ok(None);
+        // not a single piece of DWARF evaluation result
+        return Ok(AddressEval::EvalFailed);
     }
 
     let first_piece = &eval_result[0];
     let first_piece_location = &first_piece.location;
     let gimli::Location::Address { address } = first_piece_location else {
-        return Ok(None);
+        // the piece is not an address
+        return Ok(AddressEval::EvalFailed);
     };
-    Ok(Some(*address as u32))
+    Ok(AddressEval::Address(*address as u32))
 }
