6db2d66622d9b0cc632790583143a713af7f99f2
Jan Onderka <onderjan@onderjan.net>
2026-07-18 18:54:33 +0200

    Adsd start of low (IIR construction) context

--- core/machine-check-machine/src/wir/context.rs
+++ core/machine-check-machine/src/wir/context.rs
@@ -1,8 +1,10 @@
 mod inference;
 mod inferred;
+mod low;
 mod typedef;
 mod types;
 
 pub use inference::WInferenceContext;
 pub use inferred::WInferredContext;
+pub use low::WLowContext;
 pub use types::*;
